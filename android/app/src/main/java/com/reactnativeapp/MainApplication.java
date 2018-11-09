930df87f77a25e9c8fd5d480dc0d9439dcb4d7f3
srulfi <santiagorulfi@gmail.com>
2018-11-08 22:01:52 -0300

    android: integrate firebase auth

--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -3,7 +3,9 @@ package com.reactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new VectorIconsPackage(),
           new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(),
           new RNFirebaseDatabasePackage()
       );
     }
